8c5cb60ab652539405c9e93e0c7550107892590e
Chim <i.m.chernyshev@gmail.com>
2025-04-29 14:24:44 -0400

    Bird moves

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 let gravity = 0.25;
 let bird_dy = 0;
 let score = 0;
-let game_start = "Start";
+let game_state = "Start";
 
 // interval
 let gameInterval = null;
 
-let bird = document.getElementsByID("bird");
+let bird = document.getElementById("bird");
 let score_display = document.getElementById("score");
-let game_container = document.getElementById("gamce-container");
+let game_container = document.getElementById("game_container");
 let start_btn = document.getElementById("start-btn");
-
-document.addEventListener("keydown", (e) => {
-    if (e.code === "Space" ) {
-        if (game_state !== "Play") {
-            game_state = "Play";
-            startGame();
-        }
-    }
-
-    bird_dy = -7;
-})
 
 function applyGravity() {
   bird_dy += gravity;
@@ -29,13 +18,29 @@ function applyGravity() {
   birdTop = Math.max(birdTop, 0); // if (bridtop < 0) {birdTop = 0};
   birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
 
-  bird.style.yop = birdTop + "px";
+  bird.style.top = birdTop + "px";
 }
 
 function startGame() {
-    if (gameInterval !== null) return;
+  if (gameInterval !== null) return;
 
-    gameInterval = setInterval(() => {
-        applyGravity()
-    })
+  gameInterval = setInterval(() => {
+    applyGravity();
+  }, 10);
 }
+function onStartButtonClick(){
+    if (game_state !== "Play") {
+        game_state = "Play";
+        startGame()
+    }
+}
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" || e.code === "ArrowUp") {
+    if (game_state !== "Play") {
+      game_state = "Play";
+      startGame();
+    }
+
+    bird_dy = -7;
+  }
+});
