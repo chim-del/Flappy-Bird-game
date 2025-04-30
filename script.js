961c148b992e9fa2a2212a6127e3e5794149a54b
Chim <i.m.chernyshev@gmail.com>
2025-04-30 11:31:57 -0400

    Pipes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let gravity = 0.25;
 let bird_dy = 0;
 let score = 0;
 let game_state = "Start";
+let pipes = [];
+let pipe_gap = 250;
+let frame = 0;
+const frame_time = 150;
 
 // interval
 let gameInterval = null;
@@ -26,6 +30,11 @@ function startGame() {
 
   gameInterval = setInterval(() => {
     applyGravity();
+    movePipes();
+    frame++;
+    if (frame % frame_time === 0) {
+      createPipe();
+    }
   }, 10);
 }
 function onStartButtonClick(){
@@ -39,3 +48,43 @@ document.addEventListener("keydown", (e) => {
     bird_dy = -7;
   }
 });
+
+// Create Pipe
+function createPipe() {
+  let pipe_position = 
+    Math.floor(Math.random() * (game_container.offsetHeight - pipe_gap - 100)) + 
+    50;
+
+  // Top pipe
+  let top_pipe = document.createElement("div");
+  top_pipe.className = "pipe top_pipe";
+  top_pipe.style.height = pipe_position + "px";
+  top_pipe.style.top = "0px";
+  top_pipe.style.left = "100%";
+  game_container.appendChild(top_pipe);
+
+  // Bottom pipe
+  let bottom_pipe = document.createElement("div");
+  bottom_pipe.className = "pipe bottom_pipe";
+  bottom_pipe.style.height = 
+    game_container.offsetHeight - pipe_gap - pipe_position + "px";
+  bottom_pipe.style.bottom = "0px";
+  bottom_pipe.style.left = "100%"
+  game_container.appendChild(bottom_pipe);
+
+  pipes.push(top_pipe, bottom_pipe);
+}
+
+function movePipes() {
+  for (let pipe of pipes) {
+    pipe.style.left = pipe.offsetLeft - 3 + "px";
+
+    // Remove pipes from the screeen
+    if (pipe.offsetLeft < -pipe.offsetWidth) {
+      pipe.remove()
+    }
+  }
+
+// Remove old pipes from the array
+pipes = pipes.filter((pipe) => pipe.offsetLeft + pipe.offsetWidth > 0);
+}
